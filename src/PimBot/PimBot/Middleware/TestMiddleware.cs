01b4dbda39b9250aa6470b8d26ca0cfe590c955a
Miroslav Novak <miroslav.novak@lundegaard.eu>
2019-01-05 11:11:50 +0100

    add base bot structure

---
+++ src/PimBot/PimBot/Middleware/TestMiddleware.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Bot.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PimBot.Middleware
+{
+    public class TestMiddleware : IMiddleware
+    {
+        public async Task OnTurnAsync(ITurnContext context, NextDelegate next, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            await context.SendActivityAsync($"[TestMiddleware] {context.Activity.Type}/OnTurn/Before");
+
+            await next(cancellationToken);
+
+            await context.SendActivityAsync($"[TestMiddleware] {context.Activity.Type}/OnTurn/After");
+
+        }
+    }
+}
---
+++ src/PimBot/PimBot/State/StateCounter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PimBot.State
+{
+    public class StateCounter
+    {
+        /// <summary>
+        /// Gets or sets the number of turns in the conversation.
+        /// </summary>
+        /// <value>The number of turns in the conversation.</value>
+        public int TurnCount { get; set; } = 0;
+    }
+}
