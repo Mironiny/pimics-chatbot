927ca6f1de7565e3da98d72606b9a93eceb79247
Miroslav Novak <miroslav.novak@lundegaard.eu>
2019-02-04 15:00:15 +0100

    conencted to OData server

--- src/PimBot/PimBot/Middleware/TestMiddleware.cs
+++ src/PimBot/PimBot/Middleware/TestMiddleware.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.Bot.Builder;
+﻿using FeaturesPimServiceReference;
+using Microsoft.Bot.Builder;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
+
+using Simple.OData.Client;
+
 
 namespace PimBot.Middleware
 {
@@ -15,7 +20,42 @@ namespace PimBot.Middleware
 
             await next(cancellationToken);
 
-            await context.SendActivityAsync($"[TestMiddleware] {context.Activity.Type}/OnTurn/After");
+            ODataClientSettings settings = new ODataClientSettings();
+            settings.BaseUri = new Uri("http://pimicschatbot.westeurope.cloudapp.azure.com:7048/NAV/OData/");
+
+            settings.Credentials = new NetworkCredential("Allium", "#Allium12345$");
+
+            var client = new ODataClient(settings);
+
+            var featuresPIM = await client
+                .For("FeaturesPIM")
+                .FindEntriesAsync();
+
+
+            var e = featuresPIM.First();
+
+            var s = e["Code"];
+
+
+            //            FeaturesPIM featuresPim = new FeaturesPIM();
+
+            //            FeaturesPIM_PortClient featuresPimPortClient = new FeaturesPIM_PortClient();
+            //
+            //            featuresPimPortClient.featuresPimPortClient.CreateAsync();
+
+
+            //            var featuresPimPort = new FeaturesPIM_PortClient(new Uri("http://pimicschatbot.westeurope.cloudapp.azure.com:7047/NAV/WS/CRONUS%20International%20Ltd"));
+
+
+            //           var result1 = await featuresPimPort.ReadByRecIdAsync("0");
+            //       var result = await featuresPimPort.ReadAsync("INTERNAL", "Item", "");
+            //
+            //            var fuu = featuresPimPort.State;
+
+            //            var valules = result.FeaturesPIM.Values;
+
+
+            await context.SendActivityAsync($"[TestMiddleware] first code: {s} and {context.Activity.Type}/OnTurn/After");
 
         }
     }
